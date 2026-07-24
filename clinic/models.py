b17f02be6e344f0456bce4d2997c8dac149165ca
nirvanajoshi <damodarj36@gmail.com>
2026-07-24 17:00:30 +0545

    Models

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    specialty = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+class Appointment(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    status = models.CharField(max_length=20, default='scheduled')   
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.patient.name
+    
